8184a927c55ce1b0a6f951f1e85e3dc0d95521a9
Calvin <calvin.mceachron@buildium.com>
2018-01-26 01:00:26 -0500

    Limit number of images shown in media-gallery

--- src/media-gallery/media-gallery.js
+++ src/media-gallery/media-gallery.js
@@ -26,6 +26,10 @@
  * 
  * @param {Function} [onEnlarge] callback to be executed when user clicks enlarge prompt
  * 
+ * @param {Number} [limitTo] limits the number of items visible in the gallery
+ * 
+ * @param {Function} [onViewAll] if `limitTo` provided, callback to be executed when user chooses to view all items 
+ * 
  * @example
     <example name="bd-media-gallery" module="buildium.angular-elements.media-gallery">
         <file name="index.html">
@@ -48,11 +52,26 @@
                                 title: 'Super Mario Action Figure',
                                 imageUrl: 'https://images.pexels.com/photos/163036/mario-luigi-yoschi-figures-163036.jpeg?h=350&dpr=2&auto=compress&cs=tinysrgb'
                             },
+                            {
+                                fileName: 'sunset-beach-people-sunrise2.jpeg',
+                                title: 'Sunset Beach People',
+                                imageUrl: 'https://images.pexels.com/photos/40815/youth-active-jump-happy-40815.jpeg?h=350&dpr=2&auto=compress&cs=tinysrgb'
+                            },
+                            {
+                                fileName: 'woman-blowing-pink-powder2.jpeg',
+                                title: 'Woman Blowing Pink Powder',
+                                imageUrl: 'https://images.pexels.com/photos/612977/pexels-photo-612977.jpeg?h=350&dpr=2&auto=compress&cs=tinysrgb'
+                            },
+                            {
+                                fileName: 'super-mario-action-figure2.jpeg',
+                                title: 'Super Mario Action Figure',
+                                imageUrl: 'https://images.pexels.com/photos/163036/mario-luigi-yoschi-figures-163036.jpeg?h=350&dpr=2&auto=compress&cs=tinysrgb'
+                            },
                         ]
                     })
             </script>
             <section ng-controller="ExampleController as vm">
-                <bd-media-gallery media="vm.media" allow-remove="true" allow-enlarge="true"></bd-media-gallery>
+                <bd-media-gallery media="vm.media" allow-remove="true" allow-enlarge="true" limit-to="3"></bd-media-gallery>
             </section>
         </file>
     </example>
@@ -67,13 +86,15 @@ component.bindings = {
     onRemove: '&?',
     onSelect: '&?',
     allowEnlarge: '<?',
-    onEnlarge: '&?'
+    onEnlarge: '&?',
+    limitTo: '<?',
+    onViewAll: '&?'
 };
 
 component.template = `
 <div class="media-gallery">
     <div class="media-gallery__media-container"
-        ng-repeat="media in vm.media track by media.fileName">
+        ng-repeat="media in vm.media | limitTo:vm.limitTo track by media.fileName">
         <div class="media-gallery__image" 
             ng-style="{'background-image': 'url(' + media.imageUrl + ')' }"
             ng-click="vm.selectMedia(media)"> 
@@ -90,6 +111,12 @@ component.template = `
             ng-if="media.isRemovable || (media.isRemovable !== false && vm.allowRemove)"
             ng-click="vm.removeMedia(media)">
             <span class="media-gallery__screen-reader-only">Remove</span>
+        </button>
+        <button class="media-gallery__view-all"
+            ng-if="vm.limitTo < vm.media.length"
+            ng-show="$last"
+            ng-click="vm.viewAll()">
+            +{{ vm.media.length - vm.limitTo }}
         </button>
     </div>
 </div>
